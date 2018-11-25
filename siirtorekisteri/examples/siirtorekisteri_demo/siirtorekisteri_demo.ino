b3a001720cfa45c2dedf0e308b06dfde82f36684
Teneppa <laurilateemu8@gmail.com>
2018-11-25 12:46:43 +0200

    Lisätty kommentteja esimerkkikoodiin

--- siirtorekisteri/examples/siirtorekisteri_demo/siirtorekisteri_demo.ino
+++ siirtorekisteri/examples/siirtorekisteri_demo/siirtorekisteri_demo.ino
@@ -1,16 +1,38 @@
 #include <siirtorekisteri.h>
 
-siirtorekisteri rekisteri(1);
+/*
+
+Tämän esimerkkikoodin on kirjoittanut Teemu Laurila 23.11.2018
+
+Voit halutessasi tukea projektejani ja sivustoani ostamalla tekemiäni siirtorekisteripiirejä:
+https://teukka.webnode.com/l/tuotesuunnittelu-piirilevy-siirtorekisterille/
+
+*/
+
+siirtorekisteri rekisteri(1); // Luo olio siirtorekisterille ja määritä rekisterien määrä
 
 void setup() {  
-  rekisteri.aloita(3,4,5);
+  rekisteri.aloita(3,4,5); // Aseta pinnit ulostuloksi ja määritä mitä pinnejä käytetään mihinkin
+  /*
+  Tässä tapauksessa latch = D3,
+                    kellosignaali = D4 ja
+                    data = D5
+
+  Latch on STCP, eli pinni 12 piiriltä.
+  Kellosignaali on SHCP, eli pinni 11 piiriltä.
+  Data on DS, eli pinni 14 piiriltä.
+  
+  */
 }
 
 void loop() {
-  for(int x=0; x<8*rekisteri.count; x++) {
-    rekisteri.aseta(x,1);   //Aseta pinni 0 tilaan 1 (päällä)
-    delay(200);   //Odota 200 millisekuntia
-    rekisteri.aseta(x,0); //Aseta pinni 0 tilaan 0 (pois päältä)
-    delay(0);   //Odota 200 millisekuntia
-  }
+	
+	//Käy kaikki ledit läpi yksitellen
+	
+	for(int x=0; x<8*rekisteri.count; x++) {
+		rekisteri.aseta(x,1);   //Aseta pinni 0 tilaan 1 (päällä)
+		delay(200);   			//Odota 200 millisekuntia
+		rekisteri.aseta(x,0); 	//Aseta pinni 0 tilaan 0 (pois päältä)
+	}
+	
 }
